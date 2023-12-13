9e951fca516004c1c40d622a02284a0caa3b6e15
sadasdasasd <32637304+avijxxx@users.noreply.github.com>
2023-12-13 19:48:03 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -3,7 +3,7 @@
 rm -rf $(find ./feeds/luci/ -type d -regex ".*\(mosdns\|alist\|passwall\|argon\).*")
 rm -rf $(find ./feeds/packages/ -type d -regex ".*\(alist\|mosdns\).*")
 #更改默认地址为192.168.2.1
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_generate
 
 #根据源码来修改 openwrt-21.02
 if [[ $REPO_URL == *"immortalwrt-mt798x"* ]] ; then
@@ -11,9 +11,9 @@ if [[ $REPO_URL == *"immortalwrt-mt798x"* ]] ; then
   rm -rf feeds/packages/lang/golang
   git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
   #闪存频率_26MHZ修复
-  if [[ $USE_52MHZ == "false" ]] ; then
-     sed -i -e '/bus-width = <8>;/ a\    cap-mmc-highspeed;' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m-emmc.dts         
-  fi
+  #if [[ $USE_52MHZ == "false" ]] ; then
+  #   sed -i -e '/bus-width = <8>;/ a\    cap-mmc-highspeed;' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m-emmc.dts         
+  #fi
   #修改默认WIFI名
   if [[ $MODIFY_WIFI == "true" ]] ; then
       WIFI_FILE="./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh"
@@ -21,10 +21,10 @@ if [[ $REPO_URL == *"immortalwrt-mt798x"* ]] ; then
       sed -i "s/ssid=\"ImmortalWrt-5G\"/ssid=\"$USE_WIFI-5G\"/" $WIFI_FILE
   fi
   #配置文件修改_关闭内存优化_512M推荐关闭
-  if [[ $USE_MEMORY_SHRINK == "false" ]] ; then
-      AX3000_CONFIG="./defconfig/mt7981-ax3000.config"
-      MTWIFI_CONFIG="./defconfig/mt7981-ax3000-mtwifi-cfg.config"
-      sed -i '/CONFIG_MTK_MEMORY_SHRINK/d' $AX3000_CONFIG
-      sed -i '/CONFIG_MTK_MEMORY_SHRINK/d' $MTWIFI_CONFIG
-  fi
+  #if [[ $USE_MEMORY_SHRINK == "false" ]] ; then
+  #    AX3000_CONFIG="./defconfig/mt7981-ax3000.config"
+  #    MTWIFI_CONFIG="./defconfig/mt7981-ax3000-mtwifi-cfg.config"
+   #   sed -i '/CONFIG_MTK_MEMORY_SHRINK/d' $AX3000_CONFIG
+  #    sed -i '/CONFIG_MTK_MEMORY_SHRINK/d' $MTWIFI_CONFIG
+  #fi
 fi
